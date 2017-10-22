4e5f36b5b364b3dcd9caa607e20c8120f4fd7010
Davide Ciadamidaro <ciadamidaro.davide@gmail.com>
2017-10-22 18:57:23 +0200

    base for enemy control

--- Assets/Scripts/Enemies Scripts/EnemyController.cs
+++ Assets/Scripts/Enemies Scripts/EnemyController.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-    public Transform farEnd;
-    private Vector3 frometh;
-    private Vector3 untoeth;
-    private float secondsForOneLength = 20f;
+    public Transform endPosition;
+    public float secondsForOneLength = 5f;
+    public float mSpeed = 10f;
+    public bool controlled = false;
+
+    private Vector3 startPosition;
+    private SpriteRenderer spriteRenderer;
+    
 
     // Use this for initialization
     void Start () {
-        frometh = transform.position;
-        untoeth = farEnd.position;
+        startPosition = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.Lerp(frometh, untoeth, Mathf.SmoothStep(0f, 1f,Mathf.PingPong(Time.time / secondsForOneLength, 1f)));
+        if (!controlled) {
+            float lastX = transform.position.x;
+            transform.position = Vector3.Lerp(startPosition, endPosition.position, Mathf.SmoothStep(0f, 1f, Mathf.PingPong(Time.time / secondsForOneLength, 1f)));
+            if (transform.position.x > lastX)
+                spriteRenderer.flipX = false;
+            else
+                spriteRenderer.flipX = true;
+        }
+        else {
+           float mHorizontal = Input.GetAxisRaw("Horizontal");
+           float mVertical = Input.GetAxisRaw("Vertical");
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(mHorizontal*mSpeed,0f);
+
+            if(Input.GetKey(KeyCode.F)) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnDestroy() {
+        if(controlled) {
+            GameObject.FindGameObjectWithTag(Tags.player).GetComponent<GunController>().inControl = false;
+        }
     }
 }
--- Assets/Scripts/Gun Scripts/GunController.cs
+++ Assets/Scripts/Gun Scripts/GunController.cs
@@ -9,53 +9,60 @@ public class GunController : MonoBehaviour {
     public int currentCharge = 50;
     public int maxCharge = 100;
     public LayerMask lightLayer;
+    public bool inControl = false;
 
     private Transform mTransform;
+    private EnemyController enemyControlled;
+    
 
 	void Start () {
         mTransform = GetComponent<Transform>();
 	}
 	
 	void Update () {
-		
-        //rotazione della pistola
-        if(Input.GetButton("Vertical")) {
+        if (!inControl) {
+            //rotazione della pistola
+            if (Input.GetButton("Vertical")) {
 
-            float mVertical = Input.GetAxis("Vertical");
+                float mVertical = Input.GetAxis("Vertical");
 
-            mTransform.Rotate(new Vector3(0f,0f,mVertical*TurnSpeed*Time.deltaTime));
+                mTransform.Rotate(new Vector3(0f, 0f, mVertical * TurnSpeed * Time.deltaTime));
 
-            //mantiene la sprite dell'arma nel verso giusto
-            if(mTransform.rotation.eulerAngles.z%270<90 && mTransform.rotation.eulerAngles.z%270 > 0) {
-                GetComponent<SpriteRenderer>().flipY = false;
+                //mantiene la sprite dell'arma nel verso giusto
+                if (mTransform.rotation.eulerAngles.z % 270 < 90 && mTransform.rotation.eulerAngles.z % 270 > 0) {
+                    GetComponent<SpriteRenderer>().flipY = false;
+                }
+                else {
+                    GetComponent<SpriteRenderer>().flipY = true;
+                }
             }
-            else {
-                GetComponent<SpriteRenderer>().flipY = true;
-            }
-        }
 
-        if (Input.GetButtonDown("Fire1") && currentCharge<maxCharge) {
+            if (Input.GetButtonDown("Fire1") && currentCharge < maxCharge) {
 
-            //Nummero di collisioni che il fucile percepisce
-            Collider2D[] pointsOfContact = new Collider2D[25];
-            ContactFilter2D contactFilter = new ContactFilter2D();
-            contactFilter.useTriggers = true;
-            int numberOfContacts = lineOfSight.GetComponent<PolygonCollider2D>().OverlapCollider(contactFilter, pointsOfContact);
+                //Nummero di collisioni che il fucile percepisce
+                Collider2D[] pointsOfContact = new Collider2D[25];
+                ContactFilter2D contactFilter = new ContactFilter2D();
+                contactFilter.useTriggers = true;
+                int numberOfContacts = lineOfSight.GetComponent<PolygonCollider2D>().OverlapCollider(contactFilter, pointsOfContact);
 
-            for (int i = 0; i < numberOfContacts; i++) {
-                if (pointsOfContact[i].gameObject != null && pointsOfContact[i].gameObject.CompareTag(Tags.light)) {
-                    if(InLineOfSight(pointsOfContact[i]) && !pointsOfContact[i].GetComponent<LightController>().changingStatus)
-                        pointsOfContact[i].GetComponent<LightController>().SwitchOnOff(transform);
-                    break;
+                for (int i = 0; i < numberOfContacts; i++) {
+                    if (pointsOfContact[i].gameObject != null && pointsOfContact[i].gameObject.CompareTag(Tags.light)) {
+                        if (InLineOfSight(pointsOfContact[i]) && !pointsOfContact[i].GetComponent<LightController>().changingStatus)
+                            pointsOfContact[i].GetComponent<LightController>().SwitchOnOff(transform);
+                        break;
+                    }
+                    else if (pointsOfContact[i].gameObject != null && pointsOfContact[i].gameObject.CompareTag(Tags.machinery)) {
+                        //activate the machinery
+                    }
+                    else if (pointsOfContact[i].gameObject != null && pointsOfContact[i].gameObject.CompareTag(Tags.enemy)) {
+                        enemyControlled = pointsOfContact[i].GetComponent<EnemyController>();
+                        enemyControlled.controlled = true;
+                        inControl = !inControl;
+
+                    }
+
+
                 }
-                else if(pointsOfContact[i].gameObject != null && pointsOfContact[i].gameObject.CompareTag(Tags.machinery)) {
-                    //activate the machinery
-                }
-                else if(pointsOfContact[i].gameObject != null && pointsOfContact[i].gameObject.CompareTag(Tags.enemy)) {
-                    //control the enemy
-                }
-                    
-
             }
         }
 	}
