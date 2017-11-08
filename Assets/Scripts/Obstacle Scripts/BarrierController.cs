45fddf4cd05710e5928b9538e552c1f612fb40f4
Marco Trabucchi <blackhammer94@gmail.com>
2017-11-08 12:47:27 +0100

    First draft of barrier

---
+++ Assets/Scripts/Obstacle Scripts/BarrierController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierController : MonoBehaviour {
+
+    [Header("Transform associated with child platform")]
+    [SerializeField]
+    private Transform childTransform;
+
+    [Header("Transform associated with target point")]
+    [SerializeField]
+    private Transform targetPointTransform;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //transform.localPosition = Vector3.MoveTowards();
+        }
+	}
+}
