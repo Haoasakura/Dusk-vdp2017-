68e3e078d1d0bf16c4df89dfc2510f04e16aa0b8
Unknown <tommaso.loiacono01@gmail.com>
2017-11-16 09:58:51 +0100

    2D Light Yay!!!!!!

---
+++ Assets/Scripts/Gun Scripts/GunController2D.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController2D : MonoBehaviour
+{
+
+    public float TurnSpeed = 20f;
+    public int currentCharge = 50;
+    public int maxCharge = 100;
+    public bool controlling = false;
+
+    [HideInInspector]
+    public Transform mTarget;
+    public Transform barrel;
+    public Transform laserDirection;
+    public Transform line;
+    public LayerMask gunLayer;
+
+    private Player player;
+    private Transform mTransform;
+    private EnemyController enemyControlled;
+    private LineRenderer mLineRenderer;
+
+    void Start()
+    {
+        player = GetComponentInParent<Player>();
+        mTransform = GetComponent<Transform>();
+        mLineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+
+        line.GetComponent<LineRenderer>().SetPosition(0, barrel.position);
+
+        RaycastHit2D hit = Physics2D.Linecast(barrel.position, laserDirection.position/*, gunLayer*/);
+        if (hit.collider != null)
+        {
+            line.GetComponent<LineRenderer>().SetPosition(1, hit.point);
+            mTarget = hit.transform;
+        }
+        else
+        {
+            line.GetComponent<LineRenderer>().SetPosition(1, laserDirection.position);
+            mTarget = null;
+        }
+
+        if (!player.controlling)
+        {
+            //rotazione della pistola
+            if (Input.GetButton("Vertical"))
+            {
+                float mVertical = Input.GetAxis("Vertical");
+
+                mTransform.Rotate(new Vector3(0f, 0f, mVertical * TurnSpeed * Time.deltaTime));
+
+                //mantiene la sprite dell'arma nel verso giusto
+                if (mTransform.rotation.eulerAngles.z % 270 < 90 && mTransform.rotation.eulerAngles.z % 270 > 0)
+                    GetComponent<SpriteRenderer>().flipY = false;
+                else
+                    GetComponent<SpriteRenderer>().flipY = true;
+            }
+
+            if (Input.GetButtonDown("Fire1") && currentCharge < maxCharge)
+            {
+                if (mTarget != null)
+                {
+                    if (mTarget.CompareTag(Tags.light))
+                    {
+                        LightController2D currentLight = mTarget.GetComponent<LightController2D>();
+                        if (InLineOfSight(mTarget.GetComponent<Collider2D>()) && !currentLight.changingStatus)
+                            if (currentCharge >= currentLight.lightCharge || currentLight.lightStatus)
+                                mTarget.GetComponent<LightController2D>().SwitchOnOff(transform);
+                    }
+                    else if (mTarget.CompareTag(Tags.machinery))
+                    {
+                        MachineryController currentMachinery = mTarget.GetComponent<MachineryController>();
+                        if (InLineOfSight(mTarget.GetComponent<Collider2D>()) && !currentMachinery.changingStatus)
+                            if (currentCharge >= currentMachinery.powerCharge || currentMachinery.powered)
+                                currentMachinery.SwitchOnOff(transform);
+                    }
+                    else if (mTarget.CompareTag(Tags.enemy))
+                    {
+                        enemyControlled = mTarget.GetComponent<EnemyController>();
+                        if (InLineOfSight(mTarget.GetComponent<Collider2D>()) && currentCharge >= enemyControlled.controlCost)
+                            enemyControlled.ControlledOnOff(transform);
+                    }
+                }
+            }
+        }
+    }
+
+    public bool InLineOfSight(Collider2D target)
+    {
+        if (target != null)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(barrel.position, (target.transform.position - transform.position), 1000f, gunLayer);
+            if (hit.collider != null && hit.collider.gameObject.name == target.gameObject.name)
+                return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/LightController2D.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController2D : MonoBehaviour
+{
+
+    public float switchTime = 3f;
+    public int lightCharge = 25;
+    public bool lightStatus = true;
+    public bool changingStatus = false;
+
+    public Sprite[] lightStates;
+    public GameObject absorptionEffect;
+
+    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer lightAttached;
+    private Collider2D lightCollider;
+    private SpriteMask maskAttached;
+    private GameObject particleEffect;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        lightAttached = gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
+        maskAttached = gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteMask>();
+        lightCollider = gameObject.transform.GetChild(1).gameObject.GetComponent<Collider2D>();
+
+    }
+
+    void Update()
+    {
+        if (changingStatus)
+        {
+            if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+            {
+                StopCoroutine("SwitchingOn");
+                StopCoroutine("SwitchingOff");
+                StopCoroutine("TrailingEffectOn");
+                StopCoroutine("TrailingEffectOff");
+                Destroy(particleEffect);
+                changingStatus = false;
+            }
+        }
+    }
+
+    public void SwitchOnOff(Transform gun)
+    {
+        if (!lightStatus)
+            StartCoroutine("SwitchingOn", gun);
+        else
+            StartCoroutine("SwitchingOff", gun);
+    }
+
+    IEnumerator SwitchingOn(Transform gun)
+    {
+        GunController2D gunController = gun.GetComponent<GunController2D>();
+        changingStatus = true;
+        int seconds = (int)switchTime;
+        StartCoroutine("TrailingEffectOn", gunController.barrel);
+        while (seconds > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            seconds--;
+        }
+        StopCoroutine("TrailingEffectOn");
+        Destroy(particleEffect);
+        spriteRenderer.sprite = lightStates[0];
+        lightAttached.enabled = true;
+        lightCollider.enabled = true;
+        maskAttached.enabled = true;
+        lightStatus = true;
+        gunController.currentCharge -= lightCharge;
+        changingStatus = false;
+    }
+
+    IEnumerator SwitchingOff(Transform gun)
+    {
+        GunController2D gunController = gun.GetComponent<GunController2D>();
+        changingStatus = true;
+        int seconds = (int)switchTime;
+        StartCoroutine("TrailingEffectOff", gunController.barrel);
+        while (seconds > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            seconds--;
+        }
+        StopCoroutine("TrailingEffectOff");
+        Destroy(particleEffect);
+        spriteRenderer.sprite = lightStates[1];
+        lightAttached.enabled = false;
+        lightCollider.enabled = false;
+        maskAttached.enabled = false;
+        lightStatus = false;
+        gunController.currentCharge += lightCharge;
+        changingStatus = false;
+    }
+
+    IEnumerator TrailingEffectOn(Transform gun)
+    {
+        float startTime = Time.time;
+        particleEffect = Instantiate(absorptionEffect, transform.position, transform.rotation) as GameObject;
+        while (true)
+        {
+            particleEffect.transform.position = Vector3.Lerp(gun.position, transform.position, Mathf.SmoothStep(0, 1, (Time.time - startTime) / switchTime));
+            yield return null;
+        }
+    }
+
+    IEnumerator TrailingEffectOff(Transform gun)
+    {
+        float startTime = Time.time;
+        particleEffect = Instantiate(absorptionEffect, transform.position, transform.rotation) as GameObject;
+        while (true)
+        {
+            particleEffect.transform.position = Vector3.Lerp(transform.position, gun.position, Mathf.SmoothStep(0, 1, (Time.time - startTime) / switchTime));
+            yield return null;
+        }
+    }
+}
