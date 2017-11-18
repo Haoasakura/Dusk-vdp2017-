5d65c96cccc70fd8c66345def42b3779cd2106ff
Marco Trabucchi <blackhammer94@gmail.com>
2017-11-18 18:41:26 +0100

    Finished trapdoor
    (Still moves instantaneously, do we wanna make it gradual?)

---
+++ Assets/Scripts/Obstacle Scripts/TrapdoorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapdoorController : MonoBehaviour {
+
+    private bool isOpen;
+
+    [Header("Transform for the center of rotation")]
+    [SerializeField]
+    private Transform rotationCenterTransform;
+
+    // Use this for initialization
+    void Start () {
+
+        isOpen = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.G) && !isOpen)
+        {
+            gameObject.transform.RotateAround(rotationCenterTransform.position, new Vector3(0,0,1), -90);
+            isOpen = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.G) && isOpen)
+        {
+            gameObject.transform.RotateAround(rotationCenterTransform.position, new Vector3(0, 0, 1), 90);
+            isOpen = false;
+        }
+
+    }
+}
