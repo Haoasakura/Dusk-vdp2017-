f11326d05a01dbd006037f354654437933ddbc97
Marco Trabucchi <blackhammer94@gmail.com>
2017-11-25 17:57:31 +0100

    Fixed barrier bug
    Now the barrier physics should work properly!

--- Assets/Scripts/Obstacle Scripts/BarrierController.cs
+++ Assets/Scripts/Obstacle Scripts/BarrierController.cs
@@ -52,17 +52,17 @@ public class BarrierController : MonoBehaviour {
         nextPosition = (nextPosition != startingPosition ? startingPosition : targetPosition);
     }
 
-    private void OnCollisionStay2D(Collision2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         other.transform.parent = gameObject.GetComponentInChildren<Transform>(false).GetChild(0);
-        if (System.Math.Abs(other.transform.position.x -
-            gameObject.GetComponentInChildren<Transform>(false).GetChild(0).transform.position.x) > 1.25f) {
-            other.transform.parent = null;
-        }
+        //if (System.Math.Abs(other.transform.position.x -
+        //    gameObject.GetComponentInChildren<Transform>(false).GetChild(0).transform.position.x) > 1.25f) {
+        //    other.transform.parent = null;
+        //}
 
     }
 
-    private void OnCollisionExit2D(Collision2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         other.transform.parent = null;
     }
