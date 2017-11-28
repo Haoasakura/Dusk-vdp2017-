b7c9b791f7e21558e40fd79491b7df2e10051a2d
Marco Trabucchi <blackhammer94@gmail.com>
2017-11-28 21:20:36 +0100

    Added Laser
    Can be controlled as all other machineries, interacts with enemies and player, moves at a chosen speed and activates the intermittent ray at a chosen frequency [ALL IN EDITOR WINDOW for you Matteone]

---
+++ Assets/Scripts/Obstacle Scripts/LaserController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour {
+
+    public bool active = false;
+
+    [Header("Frequency of activation of laser when active")]
+    [Range(20f,120f)]
+    public int frequency = 60;
+
+    private Vector3 startingPosition;
+    private Vector3 targetPosition;
+
+    private Vector3 nextPosition;
+
+    [Header("Laser gate Speed")]
+    [SerializeField]
+    private float speed;
+
+    [Header("Transform associated with child laser")]
+    [SerializeField]
+    private Transform childTransform;
+
+    [Header("Transforms associated with extreme points")]
+    [SerializeField]
+    private Transform startPointTransform;
+    [SerializeField]
+    private Transform endPointTransform;
+
+    void Start()
+    {
+
+        startingPosition = startPointTransform.localPosition;
+        targetPosition = endPointTransform.localPosition;
+        nextPosition = startingPosition;
+    }
+
+    void Update()
+    {
+        Move();
+        IntermittentActivation();
+    }
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPosition, speed * Time.deltaTime);
+
+        if (childTransform.localPosition == startingPosition || childTransform.localPosition == targetPosition)
+        {
+            ChangeDestination();
+        }
+    }
+
+    public void ChangeDestination()
+    {
+        nextPosition = (nextPosition != startingPosition ? startingPosition : targetPosition);
+    }
+
+    public void IntermittentActivation()
+    {
+        if (active)
+        {
+            if(Time.renderedFrameCount % frequency == 0 && GetComponentInChildren<Transform>().GetChild(0).GetComponent<LineRenderer>().enabled)
+            {
+                GetComponentInChildren<Transform>().GetChild(0).GetComponent<LineRenderer>().enabled = false;
+                GetComponentInChildren<Transform>().GetChild(0).GetComponentInChildren<Transform>().GetChild(2).
+                    GetComponent<BoxCollider2D>().enabled = false;
+            }
+            else if (Time.renderedFrameCount % frequency == 0 && !GetComponentInChildren<Transform>().GetChild(0).GetComponent<LineRenderer>().enabled)
+            {
+                GetComponentInChildren<Transform>().GetChild(0).GetComponent<LineRenderer>().enabled = true;
+                GetComponentInChildren<Transform>().GetChild(0).GetComponentInChildren<Transform>().GetChild(2).
+                    GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+        else
+        {
+            GetComponentInChildren<Transform>().GetChild(0).GetComponent<LineRenderer>().enabled = false;
+            GetComponentInChildren<Transform>().GetChild(0).GetComponentInChildren<Transform>().GetChild(2).
+                GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    public void Activate()
+    {
+        active = (active ? false : true);
+    }
+}
