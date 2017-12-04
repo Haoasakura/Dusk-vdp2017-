e75af9212a9f116f2da01be6f3b180d1ec59339d
Marco Trabucchi <blackhammer94@gmail.com>
2017-12-04 10:34:34 +0100

    Added stealth laser
    This laser checks if you're visible and zaps you only if you are.
    DO NOT PUT IT WHERE THERE ARE LIGHTS!
    IT CANNOT BE SWITCHED OFF.
    Only way to pass it BY DESIGN is to wait that it goes "over you" standing still!

---
+++ Assets/Scripts/Obstacle Scripts/StealthLaserController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealthLaserController : MonoBehaviour {
+
+    public Player player;
+
+    private Vector3 startingPosition;
+    private Vector3 targetPosition;
+
+    private Vector3 nextPosition;
+
+    [Header("Laser gate Speed")]
+    [SerializeField]
+    private float speed;
+
+    [Header("Transform associated with child laser")]
+    [SerializeField]
+    private Transform childTransform;
+
+    [Header("Transforms associated with extreme points")]
+    [SerializeField]
+    private Transform startPointTransform;
+    [SerializeField]
+    private Transform endPointTransform;
+
+    void Start()
+    {
+
+        startingPosition = startPointTransform.localPosition;
+        targetPosition = endPointTransform.localPosition;
+        nextPosition = startingPosition;
+    }
+
+    void Update()
+    {
+        Move();
+        if (!player.isVisible)
+        {
+            GetComponentInChildren<Transform>().GetChild(0).GetComponentInChildren<Transform>().GetChild(2).
+                    GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else
+        {
+            GetComponentInChildren<Transform>().GetChild(0).GetComponentInChildren<Transform>().GetChild(2).
+                    GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPosition, speed * Time.deltaTime);
+
+        if (childTransform.localPosition == startingPosition || childTransform.localPosition == targetPosition)
+        {
+            ChangeDestination();
+        }
+    }
+
+    public void ChangeDestination()
+    {
+        nextPosition = (nextPosition != startingPosition ? startingPosition : targetPosition);
+    }
+}
