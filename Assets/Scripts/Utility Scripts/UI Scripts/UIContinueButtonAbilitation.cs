0079e085d707abcc15dc7471199b291d39115e94
Marco Trabucchi <blackhammer94@gmail.com>
2018-01-03 12:22:56 +0100

    Now continue button is not interactable if there is no saved game

--- Assets/Scripts/Utility Scripts/UI Scripts/UIContinueButtonAbilitation.cs
+++ Assets/Scripts/Utility Scripts/UI Scripts/UIContinueButtonAbilitation.cs
@@ -7,7 +7,7 @@ public class UIContinueButtonAbilitation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        if (PlayerPrefs.GetInt("Scene", -1) == -1)
+        if (PlayerPrefs.HasKey("Scene"))
         {
             GameObject.Find("ContinueButton").GetComponent<Button>().interactable = false;
         }
@@ -15,7 +15,15 @@ public class UIContinueButtonAbilitation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (PlayerPrefs.GetInt("Scene", -1) == -1)
+        if (PlayerPrefs.HasKey("Scene"))
+        {
+            GameObject.Find("ContinueButton").GetComponent<Button>().interactable = false;
+        }
+    }
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("Scene"))
         {
             GameObject.Find("ContinueButton").GetComponent<Button>().interactable = false;
         }
