83ae1621b3e746cd3e254f0695530dc6b8e6453b
Marco Trabucchi <blackhammer94@gmail.com>
2018-01-03 23:42:47 +0100

    Fixed bug with "continue" button activation plus graphic bug fixes

--- Assets/Scripts/Utility Scripts/UI Scripts/UIContinueButtonAbilitation.cs
+++ Assets/Scripts/Utility Scripts/UI Scripts/UIContinueButtonAbilitation.cs
@@ -5,25 +5,9 @@ using UnityEngine.UI;
 
 public class UIContinueButtonAbilitation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        if (PlayerPrefs.HasKey("Scene"))
-        {
-            GameObject.Find("ContinueButton").GetComponent<Button>().interactable = false;
-        }
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if (PlayerPrefs.HasKey("Scene"))
-        {
-            GameObject.Find("ContinueButton").GetComponent<Button>().interactable = false;
-        }
-    }
-
     private void Awake()
     {
-        if (PlayerPrefs.HasKey("Scene"))
+        if (!PlayerPrefs.HasKey("Scene"))
         {
             GameObject.Find("ContinueButton").GetComponent<Button>().interactable = false;
         }
